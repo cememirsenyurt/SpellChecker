7a7e3133d7987b1ff151e25111bd2d9411dc44a9
Cem Emir Senyurt <104103767+cememirsenyurt@users.noreply.github.com>
2024-04-03 18:08:40 -0700

    Update WordChecker.cpp

--- WordChecker.cpp
+++ WordChecker.cpp
@@ -1,181 +1,61 @@
-// WordChecker.cpp
-//
-// ICS 46 Winter 2022
-// Project #4: Set the Controls for the Heart of the Sun
-//
-// Replace and/or augment the implementations below as needed to meet
-// the requirements.
-
 #include "WordChecker.hpp"
+#include <algorithm> // For std::sort and std::unique
 
 WordChecker::WordChecker(const Set<std::string>& words)
-    : words{words}
+    : words(words)
 {
 }
-
 
 bool WordChecker::wordExists(const std::string& word) const
 {
     return words.contains(word);
 }
 
-
 std::vector<std::string> WordChecker::findSuggestions(const std::string& word) const
 {
-    // Nonsensical code because the compiler requires the member variables
-    // 'words' to be used somewhere, or else it becomes a warning (which
-    // turns into an error).
-    
-    std::vector<std::string> suggestion;
-    bool x;
+    std::vector<std::string> suggestions;
 
-    if(word.length() >= 2)
-    {
-        for(unsigned int i = 0; i < word.length(); ++i)
-        {
-            std::string newString = word;
-            newString[i] = word[i+1];
-            newString[i+1] = word[i];
-            
-            if (std::find(suggestion.begin(), suggestion.end(), newString) != suggestion.end())
-            {
-                x = false;
-            }
-            else
-            {
-                x = true;
-            }
+    // Swapping adjacent characters
+    for (size_t i = 0; i < word.length() - 1; ++i) {
+        std::string swapped = word;
+        std::swap(swapped[i], swapped[i + 1]);
+        if (wordExists(swapped)) {
+            suggestions.push_back(swapped);
+        }
+    }
 
-            if(wordExists(newString) && x)
-            {
-                suggestion.push_back(newString);
+    // Replacing characters with alphabet letters and inserting a character
+    std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (size_t i = 0; i <= word.length(); ++i) { // Note the <= to handle insertions at the end
+        for (char ch : alphabet) {
+            // Inserting a character from the alphabet
+            std::string inserted = word.substr(0, i) + ch + word.substr(i);
+            if (wordExists(inserted)) {
+                suggestions.push_back(inserted);
             }
-            else
-            {
-
+            // Replacing a character only if i < word.length() to avoid out-of-bounds
+            if (i < word.length()) {
+                std::string replaced = word;
+                replaced[i] = ch;
+                if (wordExists(replaced)) {
+                    suggestions.push_back(replaced);
+                }
             }
         }
     }
 
-    std::string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    for(unsigned int i = 0; i < abc.length(); ++i)
-    {
-        for(unsigned int j = 0; j < word.length(); ++j)
-        {
-            std::string newString = word;
-            newString.erase(j, 1);
-            newString.insert(j, sizeof(char), abc[i]);
-           
-            if (std::find(suggestion.begin(), suggestion.end(), newString) != suggestion.end())
-                {
-                        x = false;
-                            }
-                                else
-                                    {
-                                            x = true;
-                                                }
-
-
-            if(wordExists(newString) && x)
-            {
-                suggestion.push_back(newString);
-            }
-            else
-            {
-
-            }
+    // Removing each character
+    for (size_t i = 0; i < word.length(); ++i) {
+        std::string removed = word.substr(0, i) + word.substr(i + 1);
+        if (wordExists(removed)) {
+            suggestions.push_back(removed);
         }
     }
 
-    for(unsigned int i = 0; i < word.length(); ++i)
-    {
-        std::string newString;
-        newString = word;
+    // Remove duplicate suggestions
+    std::sort(suggestions.begin(), suggestions.end());
+    auto last = std::unique(suggestions.begin(), suggestions.end());
+    suggestions.erase(last, suggestions.end());
 
-        newString.erase(i, 1);
-        
-        if (std::find(suggestion.begin(), suggestion.end(), newString) != suggestion.end())
-            {
-                    x = false;
-                        }
-                            else
-                                {
-                                        x = true;
-                                            }
-
-
-        if(wordExists(newString) && x)
-        {
-            suggestion.push_back(newString);
-        }
-        else
-        {
-
-        }
-    }
-
-    for(unsigned int i = 0; i < abc.length(); ++i)
-    {
-        for(unsigned int j = 0; j < word.length(); ++j)
-        {
-            std::string newString;
-            newString = word;
-
-            if(newString[j] != abc[i])
-            {
-                newString[j] = abc[i];
-            }
-            
-            if (std::find(suggestion.begin(), suggestion.end(), newString) != suggestion.end())
-                {
-                        x = false;
-                            }
-                                else
-                                    {
-                                            x = true;
-                                                }
-
-
-            if(wordExists(newString) && x)
-            {
-                suggestion.push_back(newString);
-            }
-            else
-            {
-
-            }
-        }
-    }
-    
-    for(unsigned int i = 0; i < word.length(); ++i)
-    {
-        std::string newString;
-        std::string splited1;
-        std::string splited2;
-
-        newString = word;
-        splited1 = word.substr(0, i);
-        splited2 = word.substr(i, newString.length());
-        
-        if (std::find(suggestion.begin(), suggestion.end(), newString) != suggestion.end())
-            {
-                    x = false;
-                        }
-                            else
-                                {
-                                        x = true;
-                                            }
-
-
-        if(wordExists(splited1) && wordExists(splited2) && x)
-        {
-            std::string newVersion;
-            newVersion = splited1 + " " + splited2;
-            suggestion.push_back(newVersion);
-        }
-    }
-
-    return suggestion;
+    return suggestions;
 }
-
